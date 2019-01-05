55623bb8dca4934261bf10938bbf43a59d2e638f
oleg <oleg@albekov.ru>
2019-01-06 01:29:24 +0300

    add some "parallax" effect

--- snowterm.py
+++ snowterm.py
@@ -4,6 +4,13 @@ import sys
 import time
 
 
+snowflakes = {
+    '*': 1,
+    '+': 0.8,
+    '.': 0.4,
+}
+
+
 def max_dimensions(window):
     height, width = window.getmaxyx()
     return height - 2, width - 1
@@ -11,7 +18,7 @@ def max_dimensions(window):
 
 def snowflake_char(window):
     width = max_dimensions(window)[1]
-    char = random.choice(['+', '*', '.'])
+    char = random.choice(list(snowflakes.keys()))
     position = random.randrange(1, width)
     return (0, position, char)
 
@@ -20,9 +27,11 @@ def update_snowflakes(prev, window):
     new = {}
     for (height, position), char in prev.items():
         max_height = max_dimensions(window)[0]
-        new_height = height + 1
-        if new_height > max_height or prev.get((new_height, position)):
-            new_height -= 1
+        new_height = height
+        if random.random() <= snowflakes[char]:
+            new_height += 1
+            if new_height > max_height or prev.get((new_height, position)):
+                new_height -= 1
         new[(new_height, position)] = char
     return new
 
